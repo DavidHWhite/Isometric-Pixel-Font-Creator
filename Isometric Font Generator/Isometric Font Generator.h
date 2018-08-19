7eab269197fdc91d39e6d4a0f7f4531dde37e61c
DAVIDDESKTOP\David White <davidwhite@triad.rr.com>
2018-08-19 16:16:52 -0400

    Finished primary set of header tags

--- Isometric Font Generator/Isometric Font Generator.h
+++ Isometric Font Generator/Isometric Font Generator.h
@@ -2,8 +2,17 @@
 
 #include "INIH/INIReader.h"
 
+struct tagIdentifier {
+	string tagName;
+	string defaultVal;
+};
+
 std::string convertCoords(double xin, double yin, double zin);
 
 std::string generateTop(int width, int height, std::vector<std::vector<bool>> glyph);
 
 void writeHeaders(std::ofstream files[], INIReader &reader);
+
+void writeTagList(std::vector<tagIdentifier> &tags, INIReader & reader, std::ofstream * files);
+
+void writeNameTag(std::string tag, std::ofstream * files, INIReader & reader);
