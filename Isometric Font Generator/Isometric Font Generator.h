27339b7230edcda48bee363623057e7d2e53ac1f
DAVIDDESKTOP\David White <davidwhite@triad.rr.com>
2018-08-19 14:57:06 -0400

    Moved the 4 ofstreams into an array

--- Isometric Font Generator/Isometric Font Generator.h
+++ Isometric Font Generator/Isometric Font Generator.h
@@ -6,4 +6,4 @@ std::string convertCoords(double xin, double yin, double zin);
 
 std::string generateTop(int width, int height, std::vector<std::vector<bool>> glyph);
 
-void writeHeaders(std::ofstream &topFile, std::ofstream &shadowFile, std::ofstream &sideFile, std::ofstream &frontFile, INIReader &reader);
+void writeHeaders(std::ofstream files[], INIReader &reader);
