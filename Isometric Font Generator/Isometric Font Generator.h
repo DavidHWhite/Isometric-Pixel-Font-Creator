a688637fffb4e3d071f3058a9f74c7f2154a5e04
DAVIDDESKTOP\David White <davidwhite@triad.rr.com>
2018-08-19 12:01:55 -0400

    Corrected file path, worked on headers

--- Isometric Font Generator/Isometric Font Generator.h
+++ Isometric Font Generator/Isometric Font Generator.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "INIH/INIReader.h"
+
 std::string convertCoords(double xin, double yin, double zin);
 
 std::string generateTop(int width, int height, std::vector<std::vector<bool>> glyph);
+
+void writeHeaders(std::ofstream &topFile, std::ofstream &shadowFile, std::ofstream &sideFile, std::ofstream &frontFile, INIReader &reader);
