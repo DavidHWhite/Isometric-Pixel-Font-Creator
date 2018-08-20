e270d160a4f31f890b69ad79f3cc6f97f821a290
DAVIDDESKTOP\David White <davidwhite@triad.rr.com>
2018-08-19 20:28:08 -0400

    Created writeToAll function

--- Isometric Font Generator/Isometric Font Generator.h
+++ Isometric Font Generator/Isometric Font Generator.h
@@ -16,3 +16,5 @@ void writeHeaders(std::ofstream files[], INIReader &reader);
 void writeTagList(std::vector<tagIdentifier> &tags, INIReader & reader, std::ofstream * files);
 
 void writeNameTag(std::string tag, std::ofstream * files, INIReader & reader);
+
+void writeToAll(std::ofstream * files, std::string temp);
